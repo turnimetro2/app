eab23802df64b03bc20ad1fd1767b1c4664b796f
turnimetro2 <turnimetro2@gmail.com>
2026-05-28 13:55:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -798,7 +798,7 @@ ctUsers.forEach(u => {
 });
 
 if (invalidTurns.length > 0) {
-  toast("Attenzione!\nQuesti turni non sono di ciclazione per nessun collega immesso per la data scelta:\n" 
+  toast("Attenzione!\ni seguenti turni non sono di ciclazione per nessun collega immesso per la data specificata:\n" 
         + invalidTurns.join(", "));
   // NOTIFICA NON BLOCCANTE → si prosegue comunque
 }
